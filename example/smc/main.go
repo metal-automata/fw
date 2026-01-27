c1a90b0c9b9ca22145d521c31527279d0752f6f3
Joel Rebello <mail.joelr@gmail.com>
2026-01-27 16:28:50 +0100

    example: add sample

---
+++ example/smc/main.go
@@ -0,0 +1,34 @@
+package smc
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/metal-automata/fw"
+)
+
+func main() {
+	installer := &fw.Installer{
+		//	DryRun:       true,
+		BMCAddr:      "127.0.0.1",
+		Component:    "bmc",
+		Username:     "ADMIN",
+		Password:     "hunter2",
+		Vendor:       "supermicro",
+		Version:      "1.72",
+		FirmwareFile: "files/1.72/BMC_X11AST2400-3101MS_20240730_1.72_STDsp/BMC_X11AST2400-3101MS_20240730_1.72_STDsp.bin",
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+	defer cancel()
+
+	if err := installer.Install(ctx); err != nil {
+		slog.Error("install error", slog.Any("msg", err))
+		os.Exit(1)
+	}
+
+	log.Println("done")
+}
